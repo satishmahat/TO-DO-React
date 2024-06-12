d608686437c14a76cf1aaa5e732903dda3f5afba
satishmahat <mahatsatish@gmail.com>
2024-06-12 17:39:24 +0545

    Completed

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,23 +3,38 @@ import "./todo.css"
 
 function ToDoList(){
 
-    const[tasks,setTasks] = useState(["Eat Breakfast","Code a little","And then code again"]);
+    const[tasks,setTasks] = useState([]);
     const[newTask,setNewTask] = useState("");
 
     function handleInputChange(event){
         setNewTask(event.target.value)
     }
     function addTask(){
-
+        if(newTask.trim() !==""){
+            setTasks(t => [...t,newTask]);
+        setNewTask("")
     }
-    function deleteTask(index){
+    }
 
+        
+    function deleteTask(index){
+        const updatedTasks = tasks.filter((_,i) => i !== index);
+        setTasks(updatedTasks);
     }
     function moveTaskUp(index){
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index-1]] = [updatedTasks[index-1],updatedTasks[index]];  //array destructuring
+            setTasks(updatedTasks);
+        }
 
     }
     function moveTaskDown(index){
-
+        if(index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index+1]] = [updatedTasks[index+1],updatedTasks[index]];  //array destructuring
+            setTasks(updatedTasks);
+        }
     }
 
     return(
